565d636d224e36ac1265a6065c430f240081a77d
justbridges <bridgejs@mail.uc.edu>
2024-09-17 18:10:59 -0400

    Completed Part 4

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 4;
+
+        boolean even = true;
+
+        boolean odd = false;
+
+        if (num % 2 == 0) {
+
+            System.out.println(" Number is even ");
+
+        } else {
+
+            System.out.println(" Number is odd ");
+        }
+    }
+}
